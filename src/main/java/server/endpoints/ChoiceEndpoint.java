2d0001d0aacfbb054f5c5bb795e5d9d7e3fd4e7d
srmi16 <31665735+srmi16@users.noreply.github.com>
2017-10-17 17:16:27 +0200

    Merge branch 'master' of C:\Users\Srdan Mitrovic\Desktop\QuizFML with conflicts.

--- src/main/java/server/endpoints/ChoiceEndpoint.java
+++ src/main/java/server/endpoints/ChoiceEndpoint.java
@@ -1,4 +1,61 @@
 package server.endpoints;
+//
 
-public class ChoiceEndpoint {
+import com.google.gson.Gson;
+import server.models.Choice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/Choice")
+public class   ChoiceEndpoint {
+
+    @GET
+    public Response getChoices(){
+
+        //ArrayList<Choice> choice;
+    return Response
+            .status(200)
+            .type("application/json")
+            .entity(new Gson().toJson("choice"))
+            .build();
+
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getChoiceById(@PathParam("id") int choiceID){
+
+        //Choice foundChoice
+
+        return Response
+                .status(200)
+                .type("application/json")
+                .entity(new Gson().toJson("foundChoice"))
+                .build();
+
+    }
+
+
+    @POST
+    public Response createChoice(String jsonChoice) {
+
+        Choice newChoice = new Gson().fromJson(jsonChoice, Choice.class);
+        //tilføj det nye choice til choiceArray her
+
+        return Response
+                .status(200)
+                .type("application/json")
+                .entity("{\"choiceCreated\":\"true\"}")
+                .build();
+
+
+    }
+
+
 }
+
+
