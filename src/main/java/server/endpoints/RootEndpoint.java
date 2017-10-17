df7bc9ccf4f2f2255ea940064a5d6f75332a383b
Jonathanghl <31654288+Jonathanghl@users.noreply.github.com>
2017-10-17 12:22:41 +0200

    Merge branch 'master' of /Users/Jonathan/Desktop/QuizFML with conflicts.

--- src/main/java/server/endpoints/RootEndpoint.java
+++ src/main/java/server/endpoints/RootEndpoint.java
@@ -1,10 +1,9 @@
-package server.api;
+package server.endpoints;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/")
 public class RootEndpoint {
 
     @GET
