332f200c0344ba41746a7673e1a0cdeb4a6b906d
Mads-Emil Lemb <male16ak@student.cbs.dk>
2017-10-17 10:19:05 +0200

    intial user commit

--- src/main/java/server/models/User.java
+++ src/main/java/server/models/User.java
@@ -3,5 +3,15 @@ package server.models;
 
 public class User {
 
+    private int id;
+    private String username;
+    private String password;
+    private int StudentNumber;
+    private int type;
+
+
+    public User(String username, String password, int StudentNumber)
+
+
 
 }
