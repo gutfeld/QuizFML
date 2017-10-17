7bcb1d2a78f80a6a094a04a4a4a79c835feafe64
koch94 <kristian.koch.andersen@gmail.com>
2017-10-17 20:15:22 +0200

    CourseEndpoint

--- src/main/java/server/Controllers/CourseController.java
+++ src/main/java/server/Controllers/CourseController.java
@@ -1,5 +1,21 @@
 package server.Controllers;
 
+import server.DBWrapper;
+import server.models.Course;
+
+import java.util.ArrayList;
+
 public class CourseController {
+
+
+
+    public ArrayList<Course> getCourses(){
+        DBWrapper db = new DBWrapper();
+        ArrayList<Course> courses = db.getCourses();
+
+        for(int i = 0; i < courses.size(); i++)
+        System.out.println(courses.get(i));
+        return courses;
+    }
 
 }
