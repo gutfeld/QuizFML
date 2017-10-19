7ce08e73c7cdcbfe072cf91307a242307766e0b5
ronniegejl <rogr16ab@student.cbs.dk>
2017-10-19 08:57:53 +0200

    Ikke færdig!

--- src/main/java/server/Controllers/QuestionController.java
+++ src/main/java/server/Controllers/QuestionController.java
@@ -1,4 +1,36 @@
 package server.Controllers;
+import com.google.gson.Gson;
+import server.DBWrapper;
+import server.endpoints.QuestionEndpoint;
+import server.endpoints.QuizEndpoint;
+import server.models.Question;
+import server.models.Quiz;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class QuestionController {
+    Gson gson;
+    DBWrapper db = new DBWrapper ();
+
+
+    public QuestionController(){
+        this.gson = new Gson();
+    }
+
+/*
+
+    public ArrayList <Question> getQuestions (Quiz quiz){
+        ArrayList<Question> q = db.getQuestions(quiz);
+        return q;
+
+        //for (int i = 0; i < q.size(); i++)
+            //System.out.println(q.get(i));
+        //return q;
+    }
+*/
+    public void createQuestion(Question question) throws SQLException {
+        db.createQuestion(question);
+    }
+
 }
--- src/main/java/server/endpoints/QuestionEndpoint.java
+++ src/main/java/server/endpoints/QuestionEndpoint.java
@@ -1,6 +1,8 @@
 package server.endpoints;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import server.Controllers.QuestionController;
+import server.DBWrapper;
 import server.models.Question;
 
 import javax.ws.rs.GET;
@@ -8,15 +10,20 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
-
-
-
+import java.sql.SQLException;
 
 
 @Path("/Question")
 public class QuestionEndpoint {
     @GET
-    public Response getQuestion(){
+        public Response getQuestion(String jsonQuestion) {
+
+            QuestionController controller = new QuestionController();
+            try {
+                controller.createQuestion(new Gson().fromJson(jsonQuestion, Question.class));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
 
         return Response
@@ -41,15 +48,19 @@ public class QuestionEndpoint {
     }
 
     @POST
-    public Response createQuestion(String jsonUser) {
+    public Response createQuestion(String jsonQuestion) {
 
-        Question newQuestion = new Gson().fromJson(JsonQuestion, Question.class);
-
+        QuestionController controller = new QuestionController();
+        try {
+            controller.createQuestion(new Gson().fromJson(jsonQuestion, Question.class));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return Response
                 .status(200)
                 .type("application/json")
-                .entity("{\"questionCreated\":\"true\"})
+                .entity("{\"questionCreated\":\"true\"}")
                 .build();
 
     }
