33f95713e65b02f87bd152e74523edd5f2b3e93e
Imran <imsh16ab@student.cbs.com>
2017-10-19 09:00:25 +0200

    jj

--- src/main/java/server/Controllers/ChoiceController.java
+++ src/main/java/server/Controllers/ChoiceController.java
@@ -1,4 +1,47 @@
 package server.Controllers;
 
+import com.google.gson.Gson;
+import server.DBWrapper;
+import server.models.Choice;
+import server.endpoints.ChoiceEndpoint;
+import server.models.Question;
+
+import java.util.ArrayList;
+
 public class ChoiceController {
-}
+
+        Question test;
+        Gson gson;
+        DBWrapper db = new DBWrapper();
+
+        public ChoiceController() {
+            this.gson = gson;
+        }
+
+        public ArrayList<Choice> getChoice(){
+            ArrayList<Choice> choices = db.getChoices(test);
+
+            return choices;
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
